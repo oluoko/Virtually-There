ac96889c964d00a6bf6ee6e2a1decc721faaaf03
oluoko <brianotieno586@gmail.com>
2025-02-01 10:17:13 +0300

    Virtually There: Installing NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-900">
+      <Text className="text-white text-2xl">Hello NativeWind!</Text>
+    </View>
+  );
+};
+
+export default Index;
